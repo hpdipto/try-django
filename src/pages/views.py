fc73fa283f08e81df754e36d7fdebc34ad11827a
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-24 13:19:05 +0600

    Templage tag done

--- src/pages/views.py
+++ src/pages/views.py
@@ -11,8 +11,13 @@ def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    print(request.user)
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [1, 2, 4],
+        "my_html": "<h1>Hello World</h1>"
+    }
+    return render(request, "about.html", my_context)
 
 def social_view(request, *args, **kwargs):
     print(request.user)
