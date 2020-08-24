03f51ef3fdfedc9d16db268493966a6289929d98
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-24 07:39:40 +0600

    Project initiated

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/55169cf1-e11e-4ff1-921b-b638f07b1982/Python/try_django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
