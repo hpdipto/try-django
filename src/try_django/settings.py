c68e7067ca020faa932d984a468f664f05b07be6
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-24 12:00:59 +0600

    Models changed

--- src/try_django/settings.py
+++ src/try_django/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third pary
+
+    # own
+    'products'
 ]
 
 MIDDLEWARE = [
